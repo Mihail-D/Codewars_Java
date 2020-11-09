d2a3da5ab2fcbb9262fbfd6e632f8615211c3774
Nuby <nuby.4register@gmail.com>
2020-11-09 10:40:59 +0300

    +

---
+++ 8kyu/BasicOperations.java
@@ -0,0 +1,24 @@
+public class BasicOperations {
+    public static Integer basicMath(String op, int v1, int v2) {
+        int result = 0;
+        switch (op) {
+            case "+":
+                result = v1 + v2;
+            break;
+            case "-":
+                result = v1 - v2;
+            break;
+            case "*":
+                result = v1 * v2;
+            break;
+            case "/":
+                result = v1 / v2;
+            break;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(basicMath("*", 12, 3));
+    }
+}
