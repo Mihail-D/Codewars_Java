b90f8693f1e6ab5f67aab2030822ac6a6ee7595e
Nuby <nuby.4register@gmail.com>
2021-05-20 18:25:09 +0300

    Daily commit.

---
+++ 8kyu/Grasshopper_BasicFunctionFixer.java
@@ -0,0 +1,10 @@
+public class Grasshopper_BasicFunctionFixer {
+    public static int addFive(int num) {
+        int total = num + 5;
+        return total;
+    }
+    
+    public static void main(String[] args) {
+            System.out.println(addFive(12));
+        }
+}
