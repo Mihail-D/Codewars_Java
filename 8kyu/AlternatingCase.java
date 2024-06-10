dc34e109cda3b3855a8628e972bb6bdf2f16d231
Nuby <nuby.4register@gmail.com>
2024-06-10 11:01:04 +0300

    5

---
+++ 8kyu/AlternatingCase.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AlternatingCase {
+    public static String toAlternativeString(String string) {
+        String[] arr = string.split("");
+        List<String> list = new ArrayList<>(arr.length);
+        for (String i : arr) {
+          if (i.equals(i.toLowerCase())) {
+              list.add(i.toUpperCase());
+          }
+          else if (i.equals(i.toUpperCase())) {
+              list.add(i.toLowerCase());
+          }
+        }
+
+        return String.join("", list);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(toAlternativeString("hello world"));
+        System.out.println(toAlternativeString("StringUtils.toAlternatingCase"));
+    }
+}
