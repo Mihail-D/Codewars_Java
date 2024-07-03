ac2682f01b6c28daebc93cefcaddfe6f060a76e7
Nuby <nuby.4register@gmail.com>
2024-07-03 09:52:11 +0300

    17

---
+++ 8kyu/BinToDecimal.java
@@ -0,0 +1,9 @@
+public class BinToDecimal {
+    public static int binToDecimal(String inp){
+        return Integer.parseInt(inp,2);
+    }
+    
+    public static void main(String[] args) {
+            System.out.println(binToDecimal("1001001"));
+        }
+}
