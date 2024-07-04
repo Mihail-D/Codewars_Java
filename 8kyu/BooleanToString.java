ec346985608d700271baf449613512797a7c0b34
Nuby <nuby.4register@gmail.com>
2024-07-04 10:02:03 +0300

    18

---
+++ 8kyu/BooleanToString.java
@@ -0,0 +1,9 @@
+public class BooleanToString {
+    public static String convert(boolean b) {
+        return b ? "true" : "false";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(convert(true));
+    }
+}
