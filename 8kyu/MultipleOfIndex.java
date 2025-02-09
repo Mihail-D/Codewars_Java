b2c2843c77513bc36b905904f8c976b3dd844ce3
Nuby <nuby.4register@gmail.com>
2025-02-09 21:57:43 +0300

    104

---
+++ 8kyu/MultipleOfIndex.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MultipleOfIndex {
+    public static int[] multipleOfIndex(int[] array) {
+        List<Integer> preResult = new ArrayList<>();
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] % i == 0) {
+                preResult.add(array[i]);
+            }
+        }
+        return preResult.stream().mapToInt(i -> i).toArray();
+    }
+}
