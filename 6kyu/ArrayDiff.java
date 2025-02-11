d8f69b58ba8eb7afd9bdfce5f35f1769764c5800
Nuby <nuby.4register@gmail.com>
2025-02-11 22:06:12 +0300

    106

---
+++ 6kyu/ArrayDiff.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class ArrayDiff {
+    public static int[] arrayDiff(int[] a, int[] b) {
+        // Your code here
+        return a;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(arrayDiff(new int[]{1, 2, 2}, new int[]{2})));
+    }
+}
