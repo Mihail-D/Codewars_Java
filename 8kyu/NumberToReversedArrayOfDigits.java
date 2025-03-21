229401ae40ecbec84a1a806be279ff8858ed62bf
Nuby <nuby.4register@gmail.com>
2025-03-21 19:41:20 +0300

    120

---
+++ 8kyu/NumberToReversedArrayOfDigits.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class NumberToReversedArrayOfDigits {
+    public static int[] digitize(long n) {
+        String newStr = "" + n;
+        String[] arr = newStr.split("");
+        int[] arrayOfNumbers = new int[newStr.length()];
+
+        for (int i = 0; i < arr.length; i++) {
+            arrayOfNumbers[arrayOfNumbers.length - (i + 1)] = Integer.parseInt(arr[i]);
+        }
+
+        return arrayOfNumbers;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(digitize(348597)));
+    }
+}
