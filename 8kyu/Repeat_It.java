c7b0786f3335a0f71e74bd293f99d7f88f2446b0
Nuby <nuby.4register@gmail.com>
2025-04-15 11:48:28 +0300

    139

---
+++ 8kyu/Repeat_It.java
@@ -0,0 +1,11 @@
+public class Repeat_It {
+  public static String repeatString(final Object toRepeat, final int n) {
+    return (toRepeat instanceof String)
+        ? new String(new char[n]).replace("\0", (CharSequence) toRepeat)
+        : "Not a " + "string";
+  }
+
+  public static void main(String[] args) {
+    System.out.println(repeatString(12, 11));
+  }
+}
