de5578a32ac428941b7a31e6386356f55fd7fae2
Nuby <nuby.4register@gmail.com>
2025-04-17 21:37:27 +0300

    141

---
+++ 8kyu/ReturnNegative.java
@@ -0,0 +1,12 @@
+public class ReturnNegative {
+    public static int makeNegative(final int x) {
+        return -x;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(makeNegative(510));
+    }
+}
+
+
