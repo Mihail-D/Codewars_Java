32d54c4c6262ffc70b54d6028fa51b1730be1542
Nuby <nuby.4register@gmail.com>
2025-04-25 08:48:29 +0300

    146

---
+++ 8kyu/SentenceSmash.java
@@ -0,0 +1,9 @@
+public class SentenceSmash {
+    public static String smash(String... words) {
+        return String.join(" ", words);
+    }
+    
+    public static void main(String[] args) {
+            System.out.println(smash("Bilal", "Djaghout"));
+        }
+}
