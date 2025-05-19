cbb16cbb6074c064a2489cdd168c3d08d6e5d896
Nuby <nuby.4register@gmail.com>
2025-05-19 22:44:32 +0300

    161

---
+++ 8kyu/StringTemplates.java
@@ -0,0 +1,9 @@
+public class StringTemplates {
+    public static String buildString(String... args) {
+        return "I like " + String.join(", ", args) + "!";
+    }
+    
+    public static void main(String[] args) {
+            System.out.println(buildString("Cheese","Milk","Chocolate"));
+        }
+}
