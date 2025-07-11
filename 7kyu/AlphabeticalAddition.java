aba66facc275ae035d353f8ab0d62caf3ff61cb2
Nuby <nuby.4register@gmail.com>
2025-07-11 23:13:59 +0300

    200

---
+++ 7kyu/AlphabeticalAddition.java
@@ -0,0 +1,10 @@
+public class AlphabeticalAddition {
+    public static String addLetters(String... letters) {
+        String alphabet = "zabcdefghijklmnopqrstuvwxy";
+        int sum = 0;
+        for (String str : letters) {
+            sum += str.charAt(0) - 'a' + 1;
+        }
+        return alphabet.charAt(sum % 26) + "";
+    }
+}
