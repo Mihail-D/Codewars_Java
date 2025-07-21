a63323605ecf89844baa15ce93f76ce16d625d9c
Nuby <nuby.4register@gmail.com>
2025-07-21 10:45:46 +0300

    208

---
+++ 7kyu/BasicSequencePractice.java
@@ -0,0 +1,10 @@
+public class BasicSequencePractice {
+
+    public static int[] sumOfN(int n) {
+        int[] result = new int[Math.abs(n) + 1];
+        for (int i = 1; i < result.length; i++) {
+            result[i] = result[i - 1] + (n > 0 ? i : -i);
+        }
+        return result;
+    }
+}
