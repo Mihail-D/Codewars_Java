454979accbc3b8711a19a5be527693dfc96f73eb
Nuby <nuby.4register@gmail.com>
2025-07-24 10:55:38 +0300

    211

---
+++ 7kyu/BreakingChocolateProblem.java
@@ -0,0 +1,8 @@
+public class BreakingChocolateProblem {
+    public static long breakChocolate(long n, long m) {
+        return n * m - 1;
+    }
+    public static void main(String[] args) {
+        System.out.println(breakChocolate(5, 5));
+    }
+}
